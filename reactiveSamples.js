dcf86eb5444413dee29db6efb148f68999f420dc
Vatsal Pande <vpande@ebay.com>
2019-03-09 23:27:09 -0800

    Double Click works

--- reactiveSamples.js
+++ reactiveSamples.js
@@ -1,19 +1,38 @@
 (function IIFE() {
   console.info(rxjs);
-  const { fromEvent, operators, interval } = rxjs;
-  const { map, filter, bufferWhen, debounceTime } = operators;
+  const { fromEvent, operators } = rxjs;
+  const { map, filter, delay, bufferWhen, debounceTime } = operators;
   const doubleClickBtn = document.querySelector("#doubleClick");
   const tripleClickBtn = document.querySelector("#tripleClick");
   const doubleClick$ = fromEvent(doubleClickBtn, "click");
   const tripleClick$ = fromEvent(tripleClickBtn, "click");
   // const myInterval = interval(100);
-  doubleClick$
-    // .pipe
-    // //bufferWhen(debounceTime(5000)),
-    // //map(item => item.length)
-    // ()
-    .subscribe(item => {
-      document.querySelector("#infoText").innerHTML = item;
-    });
+  /**
+   *
+   * {....->......-> .. -> ...->.... -> .......->..->} ( Stream of Clicks)
+   *      [Buffer(throttle(duration))]
+   * { ...-> ....... -> -> ....->.....->.........-> -> }
+   *  [Map To Length]
+   *  [ 1 ...........2.......1.......1............2]
+   *    [Filter length less than 2 ]
+   *  [.............2.............................2]
+   */
+  const debouncedClicksStream = doubleClick$.pipe(debounceTime(250));
+  const bufferClickStream = doubleClick$.pipe(
+    bufferWhen(() => debouncedClicksStream)
+  );
+  const bufferedClicksFilteredStream = bufferClickStream.pipe(
+    map(item => {
+      console.info(item);
+      return item.length;
+    }),
+    filter(item => item == 2)
+  );
+  bufferedClicksFilteredStream.subscribe(() => {
+    document.querySelector("#infoText").innerHTML = "Double Click Happened";
+  });
+  bufferedClicksFilteredStream.pipe(delay(1000)).subscribe(() => {
+    document.querySelector("#infoText").innerHTML = "-";
+  });
   tripleClick$.subscribe(item => console.info(item));
 })();
